824e095d3f8ae6fea359100b5d7e3ecf50def470
Narayan <vashisthnarayan98@gmail.com>
2022-12-13 16:32:54 +0530

    Question 2, 1st submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x, int y){
+            int ans;
+            return ans = x+y;
+        }
+        public int product(int x, int y, int z){
+            int ans;
+            return ans = x+y+z;
+        }
+        public double product(double x, double y){
+            double ans;
+            return ans = x+y;
+        }
+    }
+    public static void main(String[] args) {
+        int ans, ans1;
+        double sol;
+        Product p = new Product();
+        ans1 = p.product(3,4);
+        ans = p.product(3,4,5);
+        sol = p.product(3.4,5.5);
+        System.out.println(ans1);
+        System.out.println(ans);
+        System.out.println(sol);
+    }
 }
